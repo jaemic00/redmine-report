7779957d97f5d4f204f71eccf931a74fe6263f5d
MaciejWitkowskiDev <m.witkowski@otsu.pl>
2019-07-09 10:45:49 +0000

    Moved smtp connection variables to a seperate file for convenience, added said file to .gitignore

--- sendReport.sh
+++ sendReport.sh
@@ -21,6 +21,6 @@ echo "<h2>$INFO</h2>" >> ./temp/messageBody.html
 #Finishing the html tag
 echo "</html>" >> ./temp/messageBody.html
 #Sending the email
-./sendEmail -f "witkowski.you2.pl@you2.pl" -t $ADMINEMAILS -u "$SUBJECT" -s "smtp.dpoczta.pl:25" -xu "witkowski.you2.pl@you2.pl" -xp "Test1234" -o message-content-type=html -o message-file="./temp/messageBody.html" -o message-charset=utf-8
+./sendEmail -f "witkowski.you2.pl@you2.pl" -t $ADMINEMAILS -u "$SUBJECT" -s "$SMTP_SERVER" -xu "$SMTP_USER" -xp "$SMTP_PASSWORD" -o message-content-type=html -o message-file="./temp/messageBody.html" -o message-charset=utf-8
 #Removing the temp directory
 rm -r temp
