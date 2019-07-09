1641e8f67a35da291c199d29aa1dcc9f5294ec73
MaciejWitkowskiDev <m.witkowski@otsu.pl>
2019-07-09 12:45:10 +0000

    Added a personal weekly report script

---
+++ redmine_weekly_report_myentries.sh
@@ -0,0 +1,20 @@
+#Loading variables from the variables.sh file
+source variables.sh
+source smtp_config.sh
+#Variables that hold the current week and year for the message subject and headers
+week=$(date +"%V")
+year=$(date +"%Y")
+#Variable that holds the message subject and info about the report
+SUBJECT="Redmine Personal Weekly Report: Week $week of $year"
+INFO="This report contains all the time entries you submitted during week $week of $year"
+#Get user e-mails from the database
+USEREMAILS=$(psql -c "Copy (SELECT string_agg(address, ',') FROM email_addresses) To STDOUT;")
+#Generate an array of e-mails.
+IFS=', ' read -r -a mails <<< "$USEREMAILS"
+#Send a custom report to each user of his time entries.
+for mail in "${mails[@]}"
+do
+    #This is the psql command that includes the sql query.
+    PSQL_COMMAND_STRING="Copy (SELECT projects.name AS \"NazwaProjektu\", issues.subject AS \"Zagadnienie\", time_entries.spent_on AS \"Data\", time_entries.hours AS \"Liczba godzin\", time_entries.comments AS \"Komentarz\" from time_entries inner join issues ON issues.id = time_entries.issue_id inner join projects ON projects.id = time_entries.project_id inner join users ON time_entries.user_id = users.id inner join email_addresses ON email_addresses.user_id = users.id WHERE address='$mail' AND EXTRACT(week FROM time_entries.spent_on) = EXTRACT(week FROM CAST(CURRENT_TIMESTAMP AS DATE)) order by projects.id, users.id, time_entries.spent_on asc) To STDOUT With CSV HEADER DELIMITER ',';"
+    source sendReport.sh user
+done
