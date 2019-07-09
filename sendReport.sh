e06a1c20c67906e0c1b6d2d059725dc6fc47048b
MaciejWitkowskiDev <m.witkowski@otsu.pl>
2019-07-09 10:42:29 +0000

    Moved the ADMINEMAILS variable to the sendReport.sh file, implemented sending e-mails to administrators

--- sendReport.sh
+++ sendReport.sh
@@ -1,5 +1,7 @@
 #Loading variables from the variables.sh file
 source variables.sh
+#Getting the administrator e-mails from the database
+ADMINEMAILS=$(psql -c "Copy (SELECT string_agg(address, ',') FROM email_addresses INNER JOIN users ON users.id = email_addresses.user_id WHERE admin=true) To STDOUT;")
 #If temp exists, delete it.
 if [ -d "./temp" ]; then
   rm -r temp
@@ -19,6 +21,6 @@ echo "<h2>$INFO</h2>" >> ./temp/messageBody.html
 #Finishing the html tag
 echo "</html>" >> ./temp/messageBody.html
 #Sending the email
-./sendEmail -f "witkowski.you2.pl@you2.pl" -t "maciej.witkowski@neutrica.pl", -u "$SUBJECT" -s "smtp.dpoczta.pl:25" -xu "witkowski.you2.pl@you2.pl" -xp "Test1234" -o message-content-type=html -o message-file="./temp/messageBody.html" -o message-charset=utf-8
+./sendEmail -f "witkowski.you2.pl@you2.pl" -t $ADMINEMAILS -u "$SUBJECT" -s "smtp.dpoczta.pl:25" -xu "witkowski.you2.pl@you2.pl" -xp "Test1234" -o message-content-type=html -o message-file="./temp/messageBody.html" -o message-charset=utf-8
 #Removing the temp directory
 rm -r temp
